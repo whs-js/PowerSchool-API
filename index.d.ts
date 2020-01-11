6d228eb16478df18abba6814f8bf90f6e80109b9
DusterTheFirst <14093962+DusterTheFirst@users.noreply.github.com>
2020-01-11 10:39:44 -0500

    Change to multiuser support

--- index.d.ts
+++ index.d.ts
@@ -1093,7 +1093,7 @@ declare module "@whsha/powerschool-api" {
          * Get information about this account's student.
          * @return {Promise.<PowerSchoolStudentInfo[], Error>} A promise that resolves with the account's students information, and rejects with an Error if one occurred.
          */
-        getStudentInfo(): Promise<PowerSchoolStudentInfo>;
+        getStudentsInfo(): Promise<PowerSchoolStudentInfo[]>;
     }
 
     class PowerSchoolSession {
